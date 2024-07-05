f441aa710bb595df726bab676104cf7317a26da1
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-07-05 15:14:39 +0530

    Redirect to Authenticate against Entra

---
+++ src/pages/auth/entra.js
@@ -0,0 +1,16 @@
+export const GET = ({ params, redirect }) => {
+  // Load ENV variables
+  const tenant_id = import.meta.env.AZURE_AD_TENANT_ID;
+  const client_id = import.meta.env.AZURE_AD_CLIENT_ID;
+  const redirect_uri = import.meta.env.AZURE_AD_REDIRECT_URI;
+  const scope = "openid profile email offline_access";
+  // Random state
+  const state = Math.random().toString(36).replace("0.", "");
+  // Random nonce
+  const nonce = Math.random().toString(36).replace("0.", "");
+
+  // Redirect to the Azure AD login page
+  const url_template = `https://login.microsoftonline.com/${tenant_id}/oauth2/v2.0/authorize?client_id=${client_id}&response_type=id_token&response_mode=form_post&redirect_uri=${redirect_uri}&nonce=${nonce}&scope=${scope}&state=${state}`;
+
+  return redirect(url_template);
+};
