b6e4280a85502d320ae7ee5906c980c0b1e76e50
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-07-09 17:26:57 +0530

    Add Chart to show employees and how many bells they earned

--- static/js/rnr.js
+++ static/js/rnr.js
@@ -6,7 +6,33 @@ $(document).ready(function () {
       show: "home",
     },
     success: function (data) {
-      load_chart(data);
+      var dataPoint1 = [];
+      store_bell_map = data.store_bell_map;
+      for (var key in store_bell_map) {
+        dataPoint1.push({
+          label: key,
+          y: store_bell_map[key],
+        });
+      }
+      load_chart(
+        "chartContainer1",
+        dataPoint1,
+        "Bells (by Location) Handed out this month",
+      );
+      var dataPoint2 = [];
+      employee_bell_map = data.employee_bell_map;
+      employee_name_id_map = data.employee_name_id_map;
+      for (var key in employee_bell_map) {
+        dataPoint2.push({
+          label: employee_name_id_map[key],
+          y: employee_bell_map[key],
+        });
+      }
+      load_chart(
+        "chartContainer2",
+        dataPoint2,
+        "Bells (by Person) Handed out this month",
+      );
     },
     error: function (data) {
       console.log(data);
@@ -14,21 +40,13 @@ $(document).ready(function () {
   });
 });
 
-function load_chart(data) {
-  var dataPoints = [];
-  store_bell_map = data.store_bell_map;
-  for (var key in store_bell_map) {
-    dataPoints.push({
-      label: key,
-      y: store_bell_map[key],
-    });
-  }
-  var chart = new CanvasJS.Chart("chartContainer", {
+function load_chart(container, dataPoints, title) {
+  var chart = new CanvasJS.Chart(container, {
     theme: "light1",
     exportEnabled: true,
     animationEnabled: true,
     title: {
-      text: "Bells Handed out this month",
+      text: title,
     },
     data: [
       {
