54b07454c040de3939b72f2096e58c676c0632a5
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-07-09 11:54:19 +0530

    Frontend pages

--- intranet/endpoints/__init__.py
+++ intranet/endpoints/__init__.py
@@ -8,6 +8,8 @@ from intranet.endpoints.login import Login
 from intranet.endpoints.sales import Sales
 from intranet.endpoints.vendors import Vendors
 from intranet.endpoints.create_announcement import Create_Announcement
+from intranet.endpoints.announcements import Announcements
+from intranet.endpoints.reward_bells import Reward_Bells
 
 appserver.add_route(Home.as_view(), "/home")
 appserver.add_route(Login.as_view(), "/login")
@@ -16,3 +18,5 @@ appserver.add_route(Sales.as_view(), "/sales")
 appserver.add_route(Connectivity.as_view(), "/connectivity")
 appserver.add_route(Vendors.as_view(), "/vendors")
 appserver.add_route(Create_Announcement.as_view(), "/create_announcement")
+appserver.add_route(Announcements.as_view(), "/announcement/<uuid:str>")
+appserver.add_route(Reward_Bells.as_view(), "/reward_bells")
