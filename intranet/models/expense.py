ad92c35e7e6f41dc209c52f838c1485130a3e65d
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-07-10 18:08:44 +0530

    Update Expense Model

--- intranet/models/expense.py
+++ intranet/models/expense.py
@@ -13,6 +13,7 @@ class Expense:
         "Others",
         "Bill_Attached",
         "Personal_Vehicle_Dist",
+        "Vehicle_Type",
         "Reason",
     ]
 
@@ -23,7 +24,9 @@ class Expense:
         "Receipt_Attached": lambda x: "Yes" if x else "No",
     }
 
-    def __init__(self, data):
+    def __init__(self, data, cost_per_km_four_wheeler, cost_per_km_two_wheeler):
+        self.cost_per_km_four_wheeler = cost_per_km_four_wheeler
+        self.cost_per_km_two_wheeler = cost_per_km_two_wheeler
         for idx, field in enumerate(self.fields):
             setattr(self, field, data[idx])
 
@@ -44,19 +47,32 @@ class Expense:
             schema.remove(field)
         return schema
 
-    def get_total(self, cost_per_km) -> float:
-        total = 0
-        total += self.Stationary
-        total += self.Welfare_Meal
-        total += self.Promotion_Meal
-        total += self.Hotel_Rent
-        total += self.Connectivity_Charges
-        total += self.Travel_Charge
-        total += self.Others
-        total += self.Personal_Vehicle_Dist * cost_per_km
+    def get_total(self) -> float:
+        total = 0.0
+        total += float(self.Stationary)
+        total += float(self.Welfare_Meal)
+        total += float(self.Promotion_Meal)
+        total += float(self.Hotel_Rent)
+        total += float(self.Connectivity_Charges)
+        total += float(self.Travel_Charge)
+        total += float(self.Others)
+        total += self.get_personal_vehicle_comp()
         return total
 
     def is_bill_attached(self) -> str:
         if self.Bill_Attached:
             return "Yes"
         return "No"
+
+    def get_personal_vehicle_comp(self) -> float:
+        if self.Vehicle_Type == "4 Wheeler":
+            return self.Personal_Vehicle_Dist * self.cost_per_km_four_wheeler
+        elif self.Vehicle_Type == "2 Wheeler":
+            return self.Personal_Vehicle_Dist * self.cost_per_km_two_wheeler
+        else:
+            return 0.00
+
+    def get_vehicle_type(self) -> str:
+        if self.Vehicle_Type:
+            return self.Vehicle_Type
+        return "N/A"
