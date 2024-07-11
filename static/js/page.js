6c53bb2a52897067899a03cd7fad8a5db16bd7fb
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-07-11 11:04:50 +0530

    Add some dropdowns to the NavBar to club similar pages together

--- static/js/page.js
+++ static/js/page.js
@@ -5,10 +5,21 @@ $(document).ready(function () {
 
 navbar_entries = [
   ["/home", "Home"],
+  {
+    Rewards: [
+      ["/rewards", "Reward & Recognition"],
+      ["/reward/bells", "Award Bells"],
+    ],
+  },
+  {
+    Vendors: [
+      ["/vendors/vendors", "Vendor Management"],
+      ["/vendors/contracts", "Vendor Contracts"],
+      ["/vendors/payments", "Vendor Payments"],
+    ],
+  },
   ["/locations", "Location Master"],
   ["/connectivity", "Connectivity Status"],
-  ["/rnr", "Reward & Recognition"],
-  ["/vendors", "Vendor Management"],
 ];
 
 // Load the navbar
@@ -21,18 +32,48 @@ function load_navbar() {
   navbar.appendChild(ul);
   // Add the entries
   navbar_entries.forEach((entry) => {
-    li = document.createElement("li");
-    li.className = "nav-item";
-    li.setAttribute("aria-current", "page");
-    if (path === entry[0]) {
-      li.setAttribute("active", "");
+    // Check if the entry is an array or an dict
+    if (Array.isArray(entry)) {
+      // Add the entry
+      li = document.createElement("li");
+      li.className = "nav-item";
+      ul.appendChild(li);
+      a = document.createElement("a");
+      a.className = "nav-link";
+      a.href = entry[0];
+      a.innerHTML = entry[1];
+      if (entry[0] == path) {
+        a.className += " active";
+      }
+      li.appendChild(a);
+    } else {
+      // Add the dropdown
+      li = document.createElement("li");
+      li.className = "nav-item dropdown";
+      ul.appendChild(li);
+      a = document.createElement("a");
+      a.className = "nav-link dropdown-toggle";
+      a.href = "#";
+      a.id = "navbarDropdown";
+      a.role = "button";
+      a.setAttribute("data-bs-toggle", "dropdown");
+      a.setAttribute("aria-haspopup", "true");
+      a.setAttribute("aria-expanded", "false");
+      a.innerHTML = Object.keys(entry)[0];
+      li.appendChild(a);
+      // Add the dropdown menu
+      div = document.createElement("div");
+      div.className = "dropdown-menu";
+      div.setAttribute("aria-labelledby", "navbarDropdown");
+      li.appendChild(div);
+      entry[Object.keys(entry)[0]].forEach((subentry) => {
+        a = document.createElement("a");
+        a.className = "dropdown-item";
+        a.href = subentry[0];
+        a.innerHTML = subentry[1];
+        div.appendChild(a);
+      });
     }
-    ul.appendChild(li);
-    a = document.createElement("a");
-    a.className = "nav-link";
-    a.href = entry[0];
-    a.innerHTML = entry[1];
-    li.appendChild(a);
   });
   // Add the logout button
   div = document.createElement("div");
