a237eebdc9ec1df0a9b7410b5de97dd0557ca7e4
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-07-17 17:57:11 +0530

    Location Creation API

--- intranet/endpoints/api/sites/info.py
+++ intranet/endpoints/api/sites/info.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 import json
 
 from sanic.request import Request
 from sanic.response import json as json_resp
 from sanic.views import HTTPMethodView
+from sanic.log import logger
 
 from intranet.app import IntranetApp
 from intranet.decorators.require_login import require_login
@@ -67,17 +69,96 @@ class Location_Master(HTTPMethodView):
 
     @require_login(is_api=True)
     async def post(self, request: Request):
-        return json_resp({"message": "To be implemented"})
-        # data = request.json
-        # app: IntranetApp = request.app
-        # db_pool = app.get_db_pool()
-        # async with db_pool.acquire() as conn:
-        #     async with conn.cursor() as cur:
-        #         await cur.execute(
-        #             "INSERT INTO sites (name, ip) VALUES (%s, %s)",
-        #             (data["name"], data["ip"]),
-        #         )
-        # return json_resp({"message": "Location added"})
+        data = request.form
+        app: IntranetApp = request.app
+        db_pool = app.get_db_pool()
+        async with db_pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                try:
+                    storeOpenDate = data.get("storeOpenDate", None)
+                    positLiveDate = data.get("positLiveDate", None)
+                    if storeOpenDate:
+                        storeOpenDate = datetime.strptime(
+                            storeOpenDate, "%Y-%m-%d"
+                        ).strftime("%Y-%m-%d")
+                    if positLiveDate:
+                        positLiveDate = datetime.strptime(
+                            positLiveDate, "%Y-%m-%d"
+                        ).strftime("%Y-%m-%d")
+                    await cur.execute(
+                        """INSERT INTO sites (Champs_Number, Store_Code, Store_Name, Posist_Store_Name,
+                        Ownership_Type, Local_Address, City, State_Name, Region_Internal, Postal_Code,
+                        Primary_Brand_Channel, Facility_Type, Ordering_Methods, Store_Type, Store_Phone,
+                        Store_Email, Status, Latitude, Longitude, Store_Open_Date, Posit_Live_Date,
+                        Seat_Count, Local_Org_Name, Franchisee_id, Temp_Close_Date, Reopen_Date,
+                        Store_Closure_Date, Sunday_Open, Sunday_Close, Monday_Open, Monday_Close,
+                        Tuesday_Open, Tuesday_Close, Wednesday_Open, Wednesday_Close, Thursday_Open,
+                        Thursday_Close, Friday_Open, Friday_Close, Saturday_Open, Saturday_Close,
+                        Market_Name, Area_Name, Coach_ID, Ip_Range_Start, Ip_Range_End, Subnet,
+                        Static_Ip, Link_ISP, Link_Type)
+                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
+                        %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
+                        %s,%s,%s)""",
+                        (
+                            data.get("champsNumber", None),
+                            data.get("storeCode", None),
+                            data.get("storeName", None),
+                            data.get("posistStoreName", None),
+                            data.get("ownershipType", None),
+                            data.get("localAddress", None),
+                            data.get("city", None),
+                            data.get("stateName", None),
+                            data.get("regionInternal", None),
+                            data.get("postalCode", None),
+                            data.get("primaryBrandChannel", None),
+                            data.get("facilityType", None),
+                            data.get("orderingMethods", None),
+                            data.get("storeType", None),
+                            data.get("storePhone", None),
+                            data.get("storeEmail", None),
+                            data.get("status", None),
+                            data.get("latitude", None),
+                            data.get("longitude", None),
+                            storeOpenDate,
+                            positLiveDate,
+                            data.get("seatCount", None),
+                            data.get("localOrgName", None),
+                            data.get("franchiseeId", None),
+                            data.get("tempCloseDate", None),
+                            data.get("reopenDate", None),
+                            data.get("storeClosureDate", None),
+                            data.get("sundayOpen", None),
+                            data.get("sundayClose", None),
+                            data.get("mondayOpen", None),
+                            data.get("mondayClose", None),
+                            data.get("tuesdayOpen", None),
+                            data.get("tuesdayClose", None),
+                            data.get("wednesdayOpen", None),
+                            data.get("wednesdayClose", None),
+                            data.get("thursdayOpen", None),
+                            data.get("thursdayClose", None),
+                            data.get("fridayOpen", None),
+                            data.get("fridayClose", None),
+                            data.get("saturdayOpen", None),
+                            data.get("saturdayClose", None),
+                            data.get("marketName", None),
+                            data.get("areaName", None),
+                            data.get("coachId", None),
+                            data.get("ipRangeStart", None),
+                            data.get("ipRangeEnd", None),
+                            data.get("subnet", None),
+                            data.get("staticIp", None),
+                            data.get("linkISP", None),
+                            data.get("linkType", None),
+                        ),
+                    )
+                except Exception as e:
+                    logger.error(f"Failed to insert data: {e}")
+                    return json_resp(
+                        {"status": "failure", "message": "Failed to Insert"}, status=400
+                    )
+                await conn.commit()
+        return json_resp({"status": "success", "message": "Data Inserted"})
 
     @require_login(is_api=True)
     async def patch(self, request: Request):
