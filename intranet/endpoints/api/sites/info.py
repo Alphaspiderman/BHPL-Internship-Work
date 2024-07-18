e0d839b00d27bc5f3fd63aebe1429e2f64356b53
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-07-18 11:37:58 +0530

    Site Info API update for editing existing site
    Site Info API update for showing all site info

--- intranet/endpoints/api/sites/info.py
+++ intranet/endpoints/api/sites/info.py
@@ -15,6 +15,7 @@ class Location_Master(HTTPMethodView):
     @require_login(is_api=True)
     async def get(self, request: Request):
         location = request.args.get("location")
+        show_all = request.args.get("show_all")
         app: IntranetApp = request.app
         db_pool = app.get_db_pool()
         if not location:
@@ -43,20 +44,27 @@ class Location_Master(HTTPMethodView):
                         "SELECT * FROM sites WHERE Champs_Number = %s", (location,)
                     )
                     data = await cur.fetchone()
-                    location_data = [Location(data)]
-        # # Check for IT Department in JWT
+                    if data:
+                        location_data = [Location(data)]
+                    else:
+                        return json_resp(
+                            {"status": "failure", "message": "Location not found"}
+                        )
+        # Check for IT Department in JWT
         jwt_data = app.decode_jwt(request.cookies.get("JWT_TOKEN"))
         # Remove IT specific fields if the user is not from IT department
         if jwt_data["department"] != "IT":
             data = [
-                list(entry.get_data(is_IT=False).values()) for entry in location_data
+                list(entry.get_data(is_IT=False, show_all=show_all).values())
+                for entry in location_data
             ]
-            schema = location_data[0].get_schema(is_IT=False)
+            schema = location_data[0].get_schema(is_IT=False, show_all=show_all)
         else:
             data = [
-                list(entry.get_data(is_IT=True).values()) for entry in location_data
+                list(entry.get_data(is_IT=True, show_all=show_all).values())
+                for entry in location_data
             ]
-            schema = location_data[0].get_schema(is_IT=True)
+            schema = location_data[0].get_schema(is_IT=True, show_all=show_all)
 
         # Return the data using custom JSON serializer
         return json_resp(
@@ -161,15 +169,84 @@ class Location_Master(HTTPMethodView):
         return json_resp({"status": "success", "message": "Data Inserted"})
 
     @require_login(is_api=True)
-    async def patch(self, request: Request):
-        return json_resp({"message": "To be implemented"})
-        # data = request.json
-        # app: IntranetApp = request.app
-        # db_pool = app.get_db_pool()
-        # async with db_pool.acquire() as conn:
-        #     async with conn.cursor() as cur:
-        #         await cur.execute(
-        #             "UPDATE sites SET ip = %s WHERE name = %s",
-        #             (data["ip"], data["name"]),
-        #         )
-        # return json_resp({"message": "Location updated"})
+    async def put(self, request: Request):
+        data = request.form
+        app: IntranetApp = request.app
+        db_pool = app.get_db_pool()
+        async with db_pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                try:
+                    await cur.execute(
+                        """UPDATE sites SET Store_Name = %s, Posist_Store_Name = %s,
+                        Ownership_Type = %s, Local_Address = %s, City = %s, State_Name = %s,
+                        Region_Internal = %s, Postal_Code = %s, Primary_Brand_Channel = %s,
+                        Facility_Type = %s, Ordering_Methods = %s, Store_Type = %s, Store_Phone = %s,
+                        Store_Email = %s, Status = %s, Latitude = %s, Longitude = %s, Store_Open_Date = %s,
+                        Posit_Live_Date = %s, Seat_Count = %s, Local_Org_Name = %s, Franchisee_id = %s,
+                        Temp_Close_Date = %s, Reopen_Date = %s, Store_Closure_Date = %s, Sunday_Open = %s,
+                        Sunday_Close = %s, Monday_Open = %s, Monday_Close = %s, Tuesday_Open = %s,
+                        Tuesday_Close = %s, Wednesday_Open = %s, Wednesday_Close = %s, Thursday_Open = %s,
+                        Thursday_Close = %s, Friday_Open = %s, Friday_Close = %s, Saturday_Open = %s,
+                        Saturday_Close = %s, Market_Name = %s, Area_Name = %s, Coach_ID = %s,
+                        Ip_Range_Start = %s, Ip_Range_End = %s, Subnet = %s, Static_Ip = %s, Link_ISP = %s,
+                        Link_Type = %s WHERE Store_Code = %s AND Champs_Number = %s""",
+                        (
+                            data.get("storeName", None),
+                            data.get("posistStoreName", None),
+                            data.get("ownershipType", None),
+                            data.get("localAddress", None),
+                            data.get("city", None),
+                            data.get("stateName", None),
+                            data.get("regionInternal", None),
+                            data.get("postalCode", None),
+                            data.get("primaryBrandChannel", None),
+                            data.get("facilityType", None),
+                            data.get("orderingMethods", None),
+                            data.get("storeType", None),
+                            data.get("storePhone", None),
+                            data.get("storeEmail", None),
+                            data.get("status", None),
+                            data.get("latitude", None),
+                            data.get("longitude", None),
+                            data.get("storeOpenDate", None),
+                            data.get("positLiveDate", None),
+                            data.get("seatCount", None),
+                            data.get("localOrgName", None),
+                            data.get("franchiseeId", None),
+                            data.get("tempCloseDate", None),
+                            data.get("reopenDate", None),
+                            data.get("storeClosureDate", None),
+                            data.get("sundayOpen", None),
+                            data.get("sundayClose", None),
+                            data.get("mondayOpen", None),
+                            data.get("mondayClose", None),
+                            data.get("tuesdayOpen", None),
+                            data.get("tuesdayClose", None),
+                            data.get("wednesdayOpen", None),
+                            data.get("wednesdayClose", None),
+                            data.get("thursdayOpen", None),
+                            data.get("thursdayClose", None),
+                            data.get("fridayOpen", None),
+                            data.get("fridayClose", None),
+                            data.get("saturdayOpen", None),
+                            data.get("saturdayClose", None),
+                            data.get("marketName", None),
+                            data.get("areaName", None),
+                            data.get("coachId", None),
+                            data.get("ipRangeStart", None),
+                            data.get("ipRangeEnd", None),
+                            data.get("subnet", None),
+                            data.get("staticIp", None),
+                            data.get("linkISP", None),
+                            data.get("linkType", None),
+                            data.get("storeCode", None),
+                            data.get("champsNumber", None),
+                        ),
+                    )
+                except Exception as e:
+                    logger.error(f"Failed to update data: {e}")
+                    return json_resp(
+                        {"status": "failure", "message": "Failed to Update"}, status=400
+                    )
+                await conn.commit()
+        return json_resp({"status": "success", "message": "Data Updated"})
