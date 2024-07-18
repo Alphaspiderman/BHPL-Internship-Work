afbc8b0897ed2372fb5d57e573d6fe9e44c228ac
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-07-18 17:50:15 +0530

    Logout removes cookie manually by setting the expire time in the past

--- intranet/endpoints/api/logout.py
+++ intranet/endpoints/api/logout.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from sanic.request import Request
 from sanic.response import redirect
 from sanic.views import HTTPMethodView
@@ -6,5 +7,8 @@ from sanic.views import HTTPMethodView
 class Logout(HTTPMethodView):
     async def get(self, request: Request):
         rsp = redirect("/login")
-        rsp.delete_cookie("JWT_TOKEN")
+        # Force a deletion even if either server or client time has drifted
+        expire_time = datetime.now(tz=timezone.utc) - timedelta(minutes=30)
+        rsp.add_cookie("JWT_TOKEN", "", expires=expire_time, secure=False)
+        rsp.add_cookie("JWT_TOKEN", "", expires=expire_time, secure=True)
         return rsp
