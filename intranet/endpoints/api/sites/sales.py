f495ead59844b24e617449fc1beca010fe4ce23e
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-08-05 22:08:46 +0530

    Sales data API endpoint

--- intranet/endpoints/api/sites/sales.py
+++ intranet/endpoints/api/sites/sales.py
@@ -18,8 +18,13 @@ class Location_Sales(HTTPMethodView):
             return json({"status": "error", "message": "Location not provided"})
 
         # Check for date range
-        date_from = request.args.get("from")
-        date_to = request.args.get("to")
+        date_from = request.args.get("dateFrom")
+        date_to = request.args.get("dateTo")
+
+        # Check if display is for graph
+        graph = request.args.get("graph")
+        if graph:
+            graph = graph.lower() == "true"
 
         if location == "all":
             store_filter = ""
@@ -69,8 +74,87 @@ class Location_Sales(HTTPMethodView):
                     args,
                 )
                 type_data = await cur.fetchall()
+        # Convert data types
+        source_data = [
+            {
+                "Store_Id": row[0],
+                "Date": row[1].strftime("%Y-%m-%d"),
+                "Zomato_Total_Sales": float(row[2]),
+                "Zomato_Bill_Count": row[3],
+                "Swiggy_Total_Sales": float(row[4]),
+                "Swiggy_Bill_Count": row[5],
+                "POS_Total_Sales": float(row[6]),
+                "POS_Bill_Count": row[7],
+                "Kiosk_Total_Sales": float(row[8]),
+                "Kiosk_Bill_Count": row[9],
+                "Magicpin_Total_Sales": float(row[10]),
+                "Magicpin_Bill_Count": row[11],
+            }
+            for row in source_data
+        ]
+
+        type_data = [
+            {
+                "Store_Id": row[0],
+                "Date": row[1].strftime("%Y-%m-%d"),
+                "Delivery_Total_Sales": float(row[2]),
+                "Delivery_Bill_Count": row[3],
+                "Takeaway_Total_Sales": float(row[4]),
+                "Takeaway_Bill_Count": row[5],
+                "Dinein_Total_Sales": float(row[6]),
+                "Dinein_Bill_Count": row[7],
+                "Kiosk_Total_Sales": float(row[8]),
+                "Kiosk_Bill_Count": row[9],
+            }
+            for row in type_data
+        ]
+
+        # If the data is for graph, return the raw data
+        if graph:
+            return json(
+                {
+                    "status": "success",
+                    "data": {"source": source_data, "type": type_data},
+                }
+            )
+        # Sum up the values
+        source_totals = {
+            "Zomato_Total_Sales": sum(row["Zomato_Total_Sales"] for row in source_data),
+            "Swiggy_Total_Sales": sum(row["Swiggy_Total_Sales"] for row in source_data),
+            "POS_Total_Sales": sum(row["POS_Total_Sales"] for row in source_data),
+            "Kiosk_Total_Sales": sum(row["Kiosk_Total_Sales"] for row in source_data),
+            "Magicpin_Total_Sales": sum(
+                row["Magicpin_Total_Sales"] for row in source_data
+            ),
+            "Zomato_Bill_Count": sum(row["Zomato_Bill_Count"] for row in source_data),
+            "Swiggy_Bill_Count": sum(row["Swiggy_Bill_Count"] for row in source_data),
+            "POS_Bill_Count": sum(row["POS_Bill_Count"] for row in source_data),
+            "Kiosk_Bill_Count": sum(row["Kiosk_Bill_Count"] for row in source_data),
+            "Magicpin_Bill_Count": sum(
+                row["Magicpin_Bill_Count"] for row in source_data
+            ),
+        }
+
+        type_totals = {
+            "Delivery_Total_Sales": sum(
+                row["Delivery_Total_Sales"] for row in type_data
+            ),
+            "Takeaway_Total_Sales": sum(
+                row["Takeaway_Total_Sales"] for row in type_data
+            ),
+            "Dinein_Total_Sales": sum(row["Dinein_Total_Sales"] for row in type_data),
+            "Kiosk_Total_Sales": sum(row["Kiosk_Total_Sales"] for row in type_data),
+            "Delivery_Bill_Count": sum(row["Delivery_Bill_Count"] for row in type_data),
+            "Takeaway_Bill_Count": sum(row["Takeaway_Bill_Count"] for row in type_data),
+            "Dinein_Bill_Count": sum(row["Dinein_Bill_Count"] for row in type_data),
+            "Kiosk_Bill_Count": sum(row["Kiosk_Bill_Count"] for row in type_data),
+        }
+
         return json(
-            {"status": "success", "data": {"source": source_data, "type": type_data}}
+            {
+                "status": "success",
+                "data": {"source": source_totals, "type": type_totals},
+            }
         )
 
     @require_login(is_api=True)
