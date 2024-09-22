f0e51560c9eba6f5d5edf4d29461069b92205fd3
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-09-22 22:09:22 +0530

    Update Announcement creation UI

--- static/js/create_announcement.js
+++ static/js/create_announcement.js
@@ -1,5 +1,5 @@
 const initialData = {
-  name: "Title",
+  name: "Sample Title",
   about: [
     {
       insert: "Sample Announcement body.\n",
@@ -10,30 +10,68 @@ const initialData = {
 var quill;
 $(document).ready(function () {
   quill = new Quill("#quillEditor", {
-    modules: {
-      toolbar: [
-        ["bold", "italic"],
-        [{ list: "ordered" }, { list: "bullet" }],
-      ],
-    },
+    // modules: {
+    //   toolbar: [
+    //     ["bold", "italic"],
+    //     [{ list: "ordered" }, { list: "bullet" }],
+    //   ],
+    // },
     theme: "snow",
   });
 
   reset_btn = document.getElementById("resetForm");
   reset_btn.onclick = resetForm;
 
-  form = document.getElementById("createAnnouncementForm");
-  form.addEventListener("formdata", (event) => {
-    data = document.getElementsByClassName("ql-editor")[0].innerHTML;
-    event.formData.append("body", data);
+  $("#createAnnouncementForm").submit(function (e) {
+    e.preventDefault();
+    submit_form();
   });
 
   resetForm();
 });
 
+function submit_form() {
+  body = quill.getSemanticHTML();
+  dateFrom = document.querySelector("#dateFrom").value;
+  dateTo = document.querySelector("#dateTo").value;
+  title = document.querySelector("#title").value;
+  asAdmin = document.querySelector("#asAdmin").checked;
+  file = document.querySelector("#formFile").files[0];
+
+  formData = new FormData();
+
+  formData.append("title", title);
+  formData.append("body", body);
+  formData.append("date_from", dateFrom);
+  formData.append("date_to", dateTo);
+  formData.append("as_admin", asAdmin);
+  formData.append("file", file);
+
+  $.ajax({
+    type: "POST",
+    url: "/api/announcements/new",
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (data) {
+      console.log(data);
+      if (data.success) {
+        alert("Announcement created successfully");
+        resetForm();
+      } else {
+        alert(data.message);
+      }
+    },
+    error: function (data) {
+      console.log(data);
+    },
+  });
+}
+
 function resetForm() {
-  document.querySelector('[name="title"]').value = initialData.name;
-  document.querySelector('[name="date_from"]').value = "";
-  document.querySelector('[name="date_to"]').value = "";
+  data = quill.getSemanticHTML();
+  document.querySelector("#title").value = initialData.name;
+  document.querySelector("#dateFrom").value = "";
+  document.querySelector("#dateTo").value = "";
   quill.setContents(initialData.about);
 }
