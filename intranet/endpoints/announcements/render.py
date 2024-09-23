cbec8ce9bc1be0ace25c3103163847502689d740
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-09-23 23:00:15 +0530

    Update Announcement Template and Render-er

--- intranet/endpoints/announcements/render.py
+++ intranet/endpoints/announcements/render.py
@@ -20,17 +20,38 @@ class Announcements(HTTPMethodView):
                     "SELECT * FROM announcements WHERE Announcement_Id = %s", (uuid,)
                 )
                 announcements = await cur.fetchall()
-        if len(announcements) != 1:
-            return redirect("/404")
-        announcement_title = announcements[0][1]
-        announcement_body = base64.b64decode(announcements[0][2]).decode("ascii")
-        announcement_header = f"/api/files?file_id={announcements[0][5]}"
+                # Get related files
+                if len(announcements) != 1:
+                    return redirect("/404")
+                await cur.execute(
+                    "SELECT * FROM files where File_Id IN (SELECT File_Id from announcement_files WHERE Announcement_Id = %s)",  # noqa: E501
+                    (uuid,),
+                )
+                announcement_files = await cur.fetchall()
+
+        title = announcements[0][1]
+        body = base64.b64decode(announcements[0][2]).decode("ascii")
+        date_posted = announcements[0][3]
+        posted_by = announcements[0][5]
+        images = []
+        attachments = []
+        for entry in announcement_files:
+            if entry[3].startswith("image"):
+                images.append(f"/api/files?file_id={entry[0]}")
+            else:
+                attachments.append([entry[2], f"/api/files?file_id={entry[0]}"])
+
+        print(images)
+        print(attachments)
 
         return await render(
             "announcements/template.html",
             context={
-                "header_image": announcement_header,
-                "title": announcement_title,
-                "content": announcement_body,
+                "title": title,
+                "body": body,
+                "date_posted": date_posted,
+                "posted_by": posted_by,
+                "images": images,
+                "attachments": attachments,
             },
         )
